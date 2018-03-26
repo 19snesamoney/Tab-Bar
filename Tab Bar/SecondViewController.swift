ede238570a95f9201ba3f6dd265536077f32e041
sophia.nesamoney <sophia.nesamoney@gmail.com>
2018-03-26 09:20:12 -0700

    Initial Commit

---
+++ Tab Bar/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Tab Bar
+//
+//  Created by Sophia Nesamoney on 3/22/18.
+//  Copyright © 2018 Sophia Nesamoney. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
